e461cc896d12ad58cb9316085bbf65ecf1d51f2a
fkhofmann <frank.hofmann@gmx.net>
2022-02-23 22:48:18 +0100

    added parameters to addon config rewrote OCR language configuration script

--- papermerge/rootfs/etc/cont-init.d/91-OCR.sh
+++ papermerge/rootfs/etc/cont-init.d/91-OCR.sh
@@ -4,15 +4,16 @@
 # Allow OCR setting
 OCRLANG="$(bashio::config "ocrlang")"
 languageCount=$(echo "$OCRLANG" | tr -cd ',' | wc -c)
-((languageCount++))
+languageCount=$((languageCount+1))
+bashio::log.info "Configuring ${languageCount} languages"
 
 if [ -n "$OCRLANG" ]; then
     lineStart=$(sed -n '/OCR_LANGUAGES/=' /data/config/papermerge.conf.py)
     bashio::log.info "OCRLANG variable is set, processing the language packages"
     lineEnd=$(sed -n '/}/=' /data/config/papermerge.conf.py)
-
     sed -i "${lineStart},${lineEnd}d" /data/config/papermerge.conf.py
 
+    bashio::log.info "Writing new configuration"
     echo "OCRLANG = {" >> /data/config/papermerge.conf.py
 
     languages=$(echo "$OCRLANG" | tr "," "\n")
@@ -21,23 +22,25 @@ if [ -n "$OCRLANG" ]; then
 
     i=0
     for language in $languages; do
+        bashio::log.info "Processing language ${language}"
         if apt-cache show tesseract-ocr-"${language}" >/dev/null 2>&1; then
-            echo "installing tesseract-ocr-${language}" >/dev/null
+            bashio::log.info "Installing tesseract-ocr-${language}"
             apt-get install -yqq tesseract-ocr-"${language}" >/dev/null
-            languageFullName=$(apt-cache show tesseract-ocr-"${language}" | grep 'Description-en' | grep -oE '[^ ]+$')
-            ((i++))
+            languageFullName=$(apt-cache show tesseract-ocr-"${language}" | grep -E '^(Description|Description-en):' | grep -oE '[^ ]+$')
+            bashio::log.info "${language} identified as ${languageFullName}"
+            i=$((i+1))
             if [[ $i -eq $languageCount ]]; then
-                echo "  $language = $languageFullName" >> /data/config/papermerge.conf.py
+                echo "  \"$language\" : \"$languageFullName\"" >> /data/config/papermerge.conf.py
             elif [[ $i -eq 1 ]]; then
-                echo "  $language = $languageFullName," >> /data/config/papermerge.conf.py
-                bashio::log.info "setting default language to ${language}"
+                echo "  \"$language\" : \"$languageFullName\"," >> /data/config/papermerge.conf.py
+                bashio::log.info "Setting default language to ${language}"
                 sed -i "s/^OCR_DEFAULT_LANGUAGE = \"eng\"/OCR_DEFAULT_LANGUAGE = \"${language}\"/g" /data/config/papermerge.conf.py
             else
-                echo "  $language = $languageFullName," >> /data/config/papermerge.conf.py
+                echo "  \"$language\" : \"$languageFullName\"," >> /data/config/papermerge.conf.py
             fi
-            bashio::log.info "... ${i} installed"
+            bashio::log.info "... ${language} installed"
         else
-            bashio::log.info "package tesseract-ocr-${language} not found in the repository, skipping"
+            bashio::log.info "Package tesseract-ocr-${language} not found in the repository, skipping"
         fi
     done
     echo "}" >> /data/config/papermerge.conf.py
